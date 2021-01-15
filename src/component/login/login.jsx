0c9eb10b958dcde1ce4729f8afb61cda83c8f0fc
Khaleel Rahman <khaleelrahman760@gmail.com>
2021-01-15 18:35:00 +0530

    sixth commit

--- src/component/login/login.jsx
+++ src/component/login/login.jsx
@@ -7,7 +7,6 @@ import { logdata } from "./data.json";
 import * as Yup from 'yup';
 
 const LoginValidation = Yup.object().shape({
-    
     email: Yup
       .string()
       .email()
@@ -18,34 +17,22 @@ const LoginValidation = Yup.object().shape({
       .max(16)
       .required('Invalid Email or password'),
   })
-
 const Basic=(props)=>{
-   
     const[isvalid,setLogin]=useState(false);
     console.log(isvalid)
    if(localStorage.getItem('user')){
         return<Redirect from="/login" to="homepage"/>
    }
-    
-   
-    return(
-     
+    return( 
     <div className="main">
          <p className='sign'>Login</p>
          <Formik initialValues={{email:"",password:""}}
             validationSchema= {LoginValidation}
             onSubmit={(values)=>{
-           
-            
                 logdata.forEach(items=>{
                     if(items.email===values.email)
                     {
-                        
-                        
                         if(items.password===values.password){
-
-                           
-                        
                             localStorage.setItem('user',items.username)
                             if(localStorage.getItem('user')){
                             props.history.push({pathname:'/homepage'})}
@@ -53,35 +40,20 @@ const Basic=(props)=>{
                             setLogin(!isvalid)
                             props.history.push({pathname:'/login'})
                         }
-                        
                         }
                     }
-                )}
-            
-            
+                )} 
         }>
-   
-        
         <Form className='form1'>
             <Field className='un' type='email' values={logdata[0].email}  name="email" placeholder="Enter Your Email"/>
             <ErrorMessage name='email' component='div' className="err"/>
             <Field type='password' name="password" className="pass" placeholder="Enter Your Password"/>
-            
             {
                 isvalid&&<div className='err'>Invaild Email or Password</div>
             }
             <button name='submit' className="submit" type='submit' onClick={onsubmit}>Login</button>
-            
-            
-           
-           
         </Form>
-    
-
-        </Formik>
-       
+        </Formik> 
     </div>
-       
-
     )}
 export default withRouter( Basic)
